20204b2c593e3c3e7c28e8d0a1a1ce9a5380443b
Sagnik Chattopadhyaya <meshagy18@gmail.com>
2025-10-02 19:40:06 +0530

    adding kashi vrindavan english in separate section

--- js/filter.js
+++ js/filter.js
@@ -3,32 +3,24 @@ document.getElementById('searchInput').addEventListener('keyup', function() {
     let booksContainer = document.getElementById('booksContainer');
     let books = booksContainer.getElementsByClassName('single-book');
 
-    // Handle "other publisher" section
-    let otherPublisherSection = document.querySelector('.other-publisher-section');
-    let otherPublisherBooks = otherPublisherSection
-        ? otherPublisherSection.getElementsByClassName('single-book')
-        : [];
-
-    // Track if any "other publisher" book matches
-    let anyOtherPublisherVisible = false;
-
-    // First, filter all books
+    // First, filter all books (including those in other-publisher-section)
     for (let i = 0; i < books.length; i++) {
         let img = books[i].getElementsByTagName('img')[0];
         let match = img && img.alt.toUpperCase().indexOf(filter) > -1;
         books[i].style.display = match ? "" : "none";
     }
 
-    // Then, specifically check "other publisher" books
-    for (let i = 0; i < otherPublisherBooks.length; i++) {
-        if (otherPublisherBooks[i].style.display !== "none") {
-            anyOtherPublisherVisible = true;
-            break;
+    // Handle all ".other-publisher-section" elements
+    let otherPublisherSections = document.querySelectorAll('.other-publisher-section');
+    otherPublisherSections.forEach(function(section) {
+        let sectionBooks = section.getElementsByClassName('single-book');
+        let anyVisible = false;
+        for (let i = 0; i < sectionBooks.length; i++) {
+            if (sectionBooks[i].style.display !== "none") {
+                anyVisible = true;
+                break;
+            }
         }
-    }
-
-    // Show/hide the section based on matches
-    if (otherPublisherSection) {
-        otherPublisherSection.style.display = anyOtherPublisherVisible ? "" : "none";
-    }
+        section.style.display = anyVisible ? "" : "none";
+    });
 });
